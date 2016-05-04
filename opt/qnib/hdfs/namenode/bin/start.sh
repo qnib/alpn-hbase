dc5c80eb370ccdbc93df7f1b248bba7d2a174911
Christian Kniep <christian@qnib.org>
2016-05-04 21:08:52 +0200

    workaroun done w/o internet

--- opt/qnib/hdfs/namenode/bin/start.sh
+++ opt/qnib/hdfs/namenode/bin/start.sh
@@ -4,6 +4,7 @@ source /etc/bashrc.hadoop
 source /opt/qnib/consul/etc/bash_functions.sh
 
 if [ "${HADOOP_HDFS_NAMENODE}" != "true" ];then
+    echo "> 'HADOOP_HDFS_NAMENODE!=true' Remove service"
     rm -f /etc/consul.d/hdfs-namenode.json
     consul reload
     sleep 2
